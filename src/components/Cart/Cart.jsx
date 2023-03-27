9d8c025c7c7e9d9f8c42caad66b3d75518be6c4a
khanosman <khanosman1917@gmail.com>
2023-03-28 02:01:54 +0600

    added cart functionalties

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = (props) => {
-    const {cart} = props;
-    let totalPrice=0;
-    let totalShipping=0;
-    for(const product of cart){
-        totalPrice+=product.price;
-        totalShipping+=product.shipping;
+    const { cart } = props;
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        product.quantity = product.quantity || 1;
+        totalPrice += product.price * product.quantity;
+        totalShipping += product.shipping;
+        quantity += product.quantity;
     }
 
-    const tax = totalPrice*7/100;
-    const grandTotal = totalPrice+totalShipping+tax;
+    const tax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,35 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import './Shop.css';
 const Shop = () => {
 
-    const api='https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json';
-    const [products,setProducts]=useState([]);
-    const [cart,setCart]=useState([]);
+    const api = 'https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json';
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(api)
-        .then(res => res.json())
-        .then (data => setProducts(data));
-    },[]);
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
 
-    const handleAddToCart=(product)=>{
-        console.log(product);
-        const newCart =[...cart,product];
+    useEffect(() => {
+        const storeCart = getShoppingCart();
+        const savedCart = [];
+        for (const id in storeCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storeCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+               
+            }
+        }
+        setCart(savedCart);
+    }, [products]);
+
+    const handleAddToCart = (product) => {
+        //console.log(product);
+        //const newCart = [...cart, product];
+        let newCart=[];
+        const exists = cart.find(pd=>pd.id===product.id);
+        if(!exists){
+            product.quantity=1;
+            newCart = [...cart, product];
+        }
+        else{
+            exists.quantity+=1;
+            const remaining = cart.filter(pd=>pd.id!==product.id);
+            newCart=[...remaining,exists];
+        }
         setCart(newCart);
+        addToDb(product.id);
     };
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product=>  <Product key={product.id} 
+                    products.map(product => <Product key={product.id}
                         product={product}
                         handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
-               <Cart cart={cart}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
